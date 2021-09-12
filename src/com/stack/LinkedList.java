fc5732d0611b6c46c2c65c48830ca1de335e9ff2
anirudha mayya <anirudha.mayya@gmail.com>
2021-09-12 23:17:34 +0530

    Added pop operation for stack using linked list

--- src/com/stack/LinkedList.java
+++ src/com/stack/LinkedList.java
@@ -3,12 +3,16 @@ package com.stack;
 public class LinkedList <K>{
 	public INode<K> head;
 	public INode<K> tail;
-	
+
 	public LinkedList() {
 		this.head = head;
 		this.tail = tail;
 	}
-	
+
+	/**
+	 * To add into linked list
+	 * @param newNode
+	 */
 	public void add(INode<K> newNode) {
 		if(this.tail == null) {
 			this.tail = newNode;
@@ -22,7 +26,20 @@ public class LinkedList <K>{
 			this.head.setNext(tempNode);
 		}
 	}
-	
+
+	/**
+	 * To delete from linked list
+	 * @return
+	 */
+	public INode<K> pop(){
+		INode<K> tempNode = this.head;
+		this.head = head.getNext();
+		return tempNode;
+	}
+
+	/**
+	 * To print elements from linked list
+	 */
 	public void printNodes() {
 		System.out.println("My Nodes: "+head);
 	}
